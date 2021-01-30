2f52632b8e880d55057e78eae3bcb6e39ca76d92
OneGrit <myouter@outlook.com>
2021-01-30 11:51:54 +0800

    refactor(all-goals): render a list with all goals

--- src/pages/AllGoals.tsx
+++ src/pages/AllGoals.tsx
@@ -2,14 +2,32 @@ import {
   IonButtons,
   IonContent,
   IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
   IonMenuButton,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import React from "react";
+import { COURSE_DATA } from "../data/course-data";
 
 const AllGoals: React.FC = () => {
+  const goals = COURSE_DATA.map((course) => {
+    return course.goals.map((goal) => {
+      return { ...goal, courseTitle: course.title };
+    });
+  }).reduce((goalArr, nestedGoals) => {
+    let updatedGoalArray = goalArr;
+    for (const goal of nestedGoals) {
+      updatedGoalArray = updatedGoalArray.concat(goal);
+    }
+    return updatedGoalArray;
+  }, []);
+
+  console.log(goals);
+
   return (
     <IonPage>
       <IonHeader>
@@ -21,7 +39,16 @@ const AllGoals: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <h2>All goals</h2>
+        <IonList>
+          {goals.map((goal) => (
+            <IonItem key={goal.id}>
+              <IonLabel>
+                <h2>{goal.text}</h2>
+                <p>{goal.courseTitle}</p>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
